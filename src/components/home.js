c737bc1d203b73a8ef73406a73bc15020f3c9949
Eric Barbosa <ericferrari98@hotmail.com>
2023-07-14 13:44:55 -0300

    consertei o bug de imagem

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+
 const Home = ({ user, onLogout }) => {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
@@ -115,74 +116,63 @@ const Home = ({ user, onLogout }) => {
   };
 
   return (
-    <div>
-      <div className="header">
-        <div className="header-left">
-          <h2>Bagaggio</h2>
-        </div>
-        <div className="header-right">
-          <img src={user.avatar_url} alt="Avatar" className="user-avatar" />
-          <span className="user-name">{user.name}</span>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <h2>Home</h2>
-          <p>Bem-vindo, {user.name}!</p>
-          <button onClick={onLogout}>Logout</button>
-          <h3>Cadastro de Usuários</h3>
-          <input
-            type="text"
-            placeholder="Nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="CEP"
-            value={cep}
-            onChange={(e) => setCEP(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Rua"
-            value={address.street}
-            disabled
-          />
-          <input
-            type="text"
-            placeholder="Bairro"
-            value={address.neighborhood}
-            disabled
-          />
-          <input
-            type="text"
-            placeholder="Cidade"
-            value={address.city}
-            disabled
-          />
-          <input type="text" placeholder="UF" value={address.uf} disabled />
-          <button onClick={handleAddUser}>{editMode ? 'Atualizar' : 'Cadastrar'}</button>
-          <ul>
-            {users.map((user, index) => (
-              <li key={index}>
-                {user.name}
-                <br />
-                Endereço: {user.address.street}, {user.address.neighborhood}, {user.address.city} - {user.address.uf}
-                <br />
-                <button onClick={() => handleEditUser(index)}>Editar</button>
-                <button onClick={() => handleDeleteUser(index)}>Remover</button>
-              </li>
-            ))}
-          </ul>
-          {showConfirmDialog && (
-            <div>
-              <p>Tem certeza de que deseja remover o usuário?</p>
-              <button onClick={confirmDeleteUser}>Sim</button>
-              <button onClick={cancelDeleteUser}>Não</button>
-            </div>
-          )}
-        </div>
+    <div className="container">
+      <div className="content">
+        <h2>Home</h2>
+        <p>Bem-vindo, {user.name}!</p>
+        <button onClick={onLogout}>Logout</button>
+        <h3>Cadastro de Usuários</h3>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="CEP"
+          value={cep}
+          onChange={(e) => setCEP(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Rua"
+          value={address.street}
+          disabled
+        />
+        <input
+          type="text"
+          placeholder="Bairro"
+          value={address.neighborhood}
+          disabled
+        />
+        <input
+          type="text"
+          placeholder="Cidade"
+          value={address.city}
+          disabled
+        />
+        <input type="text" placeholder="UF" value={address.uf} disabled />
+        <button onClick={handleAddUser}>{editMode ? 'Atualizar' : 'Cadastrar'}</button>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>
+              {user.name}
+              <br />
+              Endereço: {user.address.street}, {user.address.neighborhood}, {user.address.city} - {user.address.uf}
+              <br />
+              <button onClick={() => handleEditUser(index)}>Editar</button>
+              <button onClick={() => handleDeleteUser(index)}>Remover</button>
+            </li>
+          ))}
+        </ul>
+        {showConfirmDialog && (
+          <div>
+            <p>Tem certeza de que deseja remover o usuário?</p>
+            <button onClick={confirmDeleteUser}>Sim</button>
+            <button onClick={cancelDeleteUser}>Não</button>
+          </div>
+        )}
       </div>
     </div>
   );
